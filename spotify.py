4d4c8eb935a4653b8ad6f5c56c905d708bc63406
olivia-cmyk <yaushi20@uw.edu>
2023-03-15 16:29:56 -0700

    v.1.0.1

--- spotify.py
+++ spotify.py
@@ -4,8 +4,6 @@ from spotipy.oauth2 import SpotifyClientCredentials
 # from lyricsgenius import Genius
 import keys
 import pprint
-import time
-
 from azapi_mine import AZlyrics
 
 # -----SETTING UP-----
@@ -37,14 +35,18 @@ def get_user_playlists(username):
 
 def get_songs_playlist(playlist_id):
     """Parameters: playlist_id = id of the playlist, string
-    Outputs: list of tuples of all the songs (artist, song name) in the playlist, up to 50 songs"""
+    Outputs: list of tuples of all the songs (artist, song name) in the playlist, up to 25 songs"""
     try:
-        songs = sp.playlist_items(playlist_id, fields="items", limit=50)
+        songs = sp.playlist_items(playlist_id, fields="items", limit=25, offset=0)
+        # pprint.pprint(songs)
         song_list = []
         for song in songs["items"]:
-            song_name = song["track"]["name"]
-            artists = song["track"]["artists"][0]["name"]
-            song_list.append((artists, song_name))
+            if song["track"] == None:
+                continue
+            else:
+                song_name = song["track"]["name"]
+                artists = song["track"]["artists"][0]["name"]
+                song_list.append((artists, song_name))
         return song_list
     except spotipy.exceptions.SpotifyException as e:
         if e.code == -1:
@@ -54,9 +56,9 @@ def get_songs_playlist(playlist_id):
 
 def get_songs_album(album_id):
     """Parameters: album_id = id of the playlist, string
-    Outputs: list of tuples of all the songs (artist, song name) in the album, up to 50 songs"""
+    Outputs: list of tuples of all the songs (artist, song name) in the album, up to 25 songs"""
     try:
-        songs = sp.album_tracks(album_id, limit=50)
+        songs = sp.album_tracks(album_id, limit=25)
         song_list = []
         for song in songs["items"]:
             song_name = song["name"]
@@ -72,25 +74,50 @@ def get_songs_album(album_id):
 def get_playlist_title(id):
     """Parameters: id = playlist id
     Outputs: string that is title of playlist"""
-    info = sp.playlist(id)
-    name = info["name"]
-    return name
+    try:
+        info = sp.playlist(id)
+        name = info["name"]
+        return name
+    except spotipy.exceptions.SpotifyException:
+        print("HTTP Error: 404. Invalid Playlist ID.")
+        return None
+
+
+def get_album_title(id):
+    """Parameters: id = album id
+    Outputs: string that is title of album"""
+    try:
+        info = sp.album(id)
+        name = info["name"]
+        return name
+    except spotipy.exceptions.SpotifyException:
+        print("HTTP Error: 404. Invalid Playlist ID.")
+        return None
 
 
 def scrape_lyrics(artist, song):
     """Parameters: artist, song = string
     Ouputs: string with lyrics, using azapi"""
-    az = AZlyrics("google", accuracy=0.1)
-    az.artist = artist
-    az.title = song
-    lyrics = az.getLyrics()  # save=True if lyrics save in txt file.
+    try:
+        az = AZlyrics("google", accuracy=0.1)
+        az.artist = artist
+        az.title = song
+        lyrics = az.getLyrics()  # save=True if lyrics save in txt file.
 
-    return lyrics
+        print("Lyrics Successfully Scraped: {}, {}".format(az.artist, az.title))
+        return lyrics
+    except:
+        print("No lyrics found.")
+        return ""
 
 
 def strip_lyrics(lyrics):
     """Parameters: lyrics = string of lyrics in song.
     Returns: lowercase string of lyrics that are stripped of punctuation"""
+    if type(lyrics) != str:
+        print("No lyrics to strip")
+        return ""
+
     punctuation = [".", ",", '"', "-", "!", "'", "(", ")", "?"]
     for mark in punctuation:
         lyrics = lyrics.replace(mark, "")
@@ -126,6 +153,37 @@ def album_common_words(id):
     """Parameters: id = id of spotify album
     Returns: 50 most common words in album"""
     songslist = get_songs_album(id)
+
+    if songslist is None:
+        print("Invalid Album ID.")
+        return None
+
+    commonwordsdict = {}
+
+    for song in songslist:
+        songname = song[1]
+        artist = song[0]
+        lyrics = scrape_lyrics(artist, songname)
+        lyrics = strip_lyrics(lyrics)
+        songworddict = no_common_words(lyrics)
+        for word in songworddict.keys():
+            if word in commonwordsdict.keys():
+                commonwordsdict[word] += songworddict[word]
+            else:
+                commonwordsdict[word] = songworddict[word]
+
+    return commonwordsdict
+
+
+def playlist_common_words(id):
+    """Parameters: id = id of spotify playlist
+    Returns: 50 most common words in playlist"""
+    songslist = get_songs_playlist(id)
+
+    if songslist is None:
+        print("Invalid Playlist ID.")
+        return None
+
     commonwordsdict = {}
 
     for song in songslist:
@@ -146,6 +204,9 @@ def album_common_words(id):
 def sort_words(commonwordsdict):
     """Parameters: dictionary of common words from playlist/album_common_words()
     Returns: list of tuples (word, count) of the 50 most popular words"""
+    if type(commonwordsdict) != dict:
+        print("Invalid Parameter, Not a Dictionary.")
+        return None
     new = [(k, v) for k, v in sorted(sorted(commonwordsdict.items(), key=lambda x:x[1], reverse=True), key=len)]
     finaldict = new[:51]
     return finaldict
@@ -153,39 +214,45 @@ def sort_words(commonwordsdict):
 
 def main():
     print("===== Testing get_user_playlists function. =====")
-    # pprint.pprint(get_user_playlists("yfm6sp0vg1zu31yc1e0s3xffd"))  # testing my username
+    pprint.pprint(get_user_playlists("yfm6sp0vg1zu31yc1e0s3xffd"))  # testing my username
     pprint.pprint(get_user_playlists("shawnmendes"))  # testing Shawn Mendes' username
     get_user_playlists("risdfrhaasdfaasdfsdfnna")  # testing gibberish username
 
     print("===== Testing get_songs_playlist function. =====")
-    pprint.pprint(get_songs_playlist("7yyTfDGxCtnzy2wwWI6xYn"))  # getting songs from Rihanna's "ANTI World Tour Set List"
+    x = (get_songs_playlist("7yyTfDGxCtnzy2wwWI6xYn"))  # getting songs from Rihanna's "ANTI World Tour Set List"
+    pprint.pprint(x)
     pprint.pprint(get_songs_playlist("gibberish"))  # testing gibberish song id
 
     print("===== Testing get_songs_album function. =====")
+    pprint.pprint(get_songs_album("151w1FgRZfnKZA9FEcg9Z3")) # getting songs from Taylor Swift's Midnights Album
     pprint.pprint(get_songs_album("gibberish"))
-    pprint.pprint(get_songs_album("151w1FgRZfnKZA9FEcg9Z3"))
 
+    print("===== Testing get_playlist_title & get_album_title function. =====")
+    print(get_playlist_title("7yyTfDGxCtnzy2wwWI6xYn"))
+    print(get_playlist_title("gibberishalsdkjf"))
+
+    print(get_album_title("151w1FgRZfnKZA9FEcg9Z3"))
+    print(get_album_title("gibberishalsdkjf"))
 
     print("===== Testing scrape_lyrics function. =====")
-    x = get_songs_playlist("7yyTfDGxCtnzy2wwWI6xYn")
-    # pprint.pprint(scrape_lyrics(x[1][0], x[1][1]))
-    # pprint.pprint(scrape_lyrics("unkwown", "gibberasidjfld"))
+    pprint.pprint(scrape_lyrics(x[1][0], x[1][1]))  # from 2nd song in Rihanna's album
+    pprint.pprint(scrape_lyrics("unkwown", "gibberasidjfld"))
 
     print("===== Testing strip_lyrics function. =====")
     lyricsx = scrape_lyrics(x[0][0], x[0][1])
-    # pprint.pprint(strip_lyrics(lyricsx))
+    pprint.pprint(strip_lyrics(lyricsx))
 
     print("===== Testing no_common_words function. =====")
     pprint.pprint(no_common_words(strip_lyrics(lyricsx)))
 
+    print("===== Testing playlist/album_common_words, sort_words function. =====")
+    a = album_common_words("151w1FgRZfnKZA9FEcg9Z3")
+    p = playlist_common_words("6OlO5RVqcA1sas9b7ztaqe")
+    print(sort_words(a))
+    print(sort_words(p))
+    gib = album_common_words("gilskjdflka;j")
+    print(sort_words(gib))
 
 
 if __name__ == "__main__":
-    # main()
-    # print(get_playlist_title("7yyTfDGxCtnzy2wwWI6xYn"))
-    # 151w1FgRZfnKZA9FEcg9Z3 midnight album
-    x = (album_common_words("151w1FgRZfnKZA9FEcg9Z3"))
-    pprint.pprint(sort_words(x))
-    # x = (scrape_lyrics("Taylor Swift", "Lavendar Haze"))
-    # y = (strip_lyrics(x))
-    # print(no_common_words(y))
+    main()
